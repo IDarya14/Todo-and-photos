e0a59d39334c75358fbc2540c5381669b2790523
IDarya14 <IDarua1401@gmail.com>
2022-04-08 09:21:23 +0300

    c

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { Home } from './components/Home';
 import { Todos } from './components/Todos';
-import { Photos } from './components/Photos';
+import { Photos } from './components/Photos/';
 import { TodoItem } from './components/TodoItem';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
